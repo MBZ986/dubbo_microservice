0965bf60fcf82c6b86f31a648c3c4a35ca576dc0
黎九桐槐 <2869748651@qq.com>
2020-12-31 17:11:15 +0800

    注释网关

--- microservice_gateway/src/main/java/cn/kgc/gateway/GateWayController.java
+++ microservice_gateway/src/main/java/cn/kgc/gateway/GateWayController.java
@@ -15,45 +15,45 @@ import javax.servlet.http.HttpServletRequest;
 @RequestMapping("/router")
 public class GateWayController {
 
-    @Reference(check = false)
-    private EmpService empService;
-
-    @Reference(check = false)
-    private DeptService deptService;
-
-    @RequestMapping("/emp/{path}")
-    public String getEmp(@PathVariable("path")String path, HttpServletRequest request){
-        String realPath = null;
-        System.out.println("调用网关路由到Emp服务");
-        String requestURI = request.getRequestURI();
-        System.out.println("路由请求地址："+requestURI);
-
-        empService.info();
-        realPath = getPath(path);
-
-        return realPath;
-    }
-
-    @RequestMapping("/dept/{path}")
-    public String getDept(@PathVariable("path")String path,HttpServletRequest request){
-        String realPath = null;
-        System.out.println("调用网关路由到Dept服务");
-        String requestURI = request.getRequestURI();
-        System.out.println("路由请求地址："+requestURI);
-
-        deptService.info();
-        realPath = getPath("dept/"+path);
-
-        return realPath;
-    }
-
-    private String getPath(String path){
-        String remoteHost = RpcContext.getContext().getRemoteHost();
-        System.out.println(remoteHost);
-
-        String realPath = String.format("http://%s:%s/%s",remoteHost,8888,path);
-        return realPath;
-    }
+//    @Reference(check = false)
+//    private EmpService empService;
+//
+//    @Reference(check = false)
+//    private DeptService deptService;
+//
+//    @RequestMapping("/emp/{path}")
+//    public String getEmp(@PathVariable("path")String path, HttpServletRequest request){
+//        String realPath = null;
+//        System.out.println("调用网关路由到Emp服务");
+//        String requestURI = request.getRequestURI();
+//        System.out.println("路由请求地址："+requestURI);
+//
+//        empService.info();
+//        realPath = getPath(path);
+//
+//        return realPath;
+//    }
+//
+//    @RequestMapping("/dept/{path}")
+//    public String getDept(@PathVariable("path")String path,HttpServletRequest request){
+//        String realPath = null;
+//        System.out.println("调用网关路由到Dept服务");
+//        String requestURI = request.getRequestURI();
+//        System.out.println("路由请求地址："+requestURI);
+//
+//        deptService.info();
+//        realPath = getPath("dept/"+path);
+//
+//        return realPath;
+//    }
+//
+//    private String getPath(String path){
+//        String remoteHost = RpcContext.getContext().getRemoteHost();
+//        System.out.println(remoteHost);
+//
+//        String realPath = String.format("http://%s:%s/%s",remoteHost,8888,path);
+//        return realPath;
+//    }
 }
 
 
